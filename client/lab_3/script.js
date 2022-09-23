9bc61d72140ba969d9bb66d0b06f7ae7781ae2db
yukibae <112517637+yukibae@users.noreply.github.com>
2022-09-23 16:46:54 -0400

    Update script.js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -24,8 +24,8 @@ function updateSlidePosition() {
     slide.classList.add('hidden');
   });
 
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible');
- 
 }
 
 function moveToNextSlide() {
@@ -35,6 +35,12 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+  
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
