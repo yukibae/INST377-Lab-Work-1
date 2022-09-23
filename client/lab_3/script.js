f838d1b84715f710beda67e34ce0cb502467b7e6
yukibae <112517637+yukibae@users.noreply.github.com>
2022-09-23 17:55:32 -0400

    prev slides

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -29,18 +29,12 @@ function updateSlidePosition() {
 }
 
 function moveToNextSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition += 1;
   }
-  
+
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -51,6 +45,12 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
+
   updateSlidePosition();
 }
 
